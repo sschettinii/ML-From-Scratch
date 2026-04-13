7bfa12f36a5d8745114ae22ed3e25ffd9c0c48ca
sschettinii <andreschettini2006@gmail.com>
2026-04-13 19:51:46 -0300

    Added metrics attribute in DBSCAN class [euclidian or manhattan]

--- DBSCAN.py
+++ DBSCAN.py
@@ -1,17 +1,21 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
+from sklearn.datasets import make_moons
+from sklearn.preprocessing import StandardScaler
 
 class DBSCAN():
 
-    def __init__(self, eps, min_samples):
+    def __init__(self, eps, min_samples, metrics):
+        assert metrics in ["euclidian", "manhattan"], "Métrica inválida"
+
         self.eps = eps
         self.min_samples = min_samples
+        self.metrics = metrics
 
         self.labels = None
 
-    def fit(self, X):
+    def fit_predict(self, X):
         n = len(X)
         cluster = 0
         self.labels = -np.ones(n)
@@ -32,16 +36,15 @@ class DBSCAN():
 
         return self
 
-   
-
     def get_neighborhood(self, p, X):
-        n, neighbors = len(X), []
-
-        for i in range(n):
-            if math.dist(p, X[i]) <= self.eps:
-                neighbors.append(i)
-
-        return neighbors
+        if self.metrics == "euclidian":
+            distances = np.linalg.norm(X - p, axis=1)
+        elif self.metrics == "manhattan":
+            distances = np.sum(np.abs(X - p), axis=1)
+        
+        neighbors = np.where(distances <= self.eps)[0]
+    
+        return neighbors.tolist()
 
     def expand_cluster(self, X, i, neighbors, cluster):
         self.labels[i], idx = cluster, 0
@@ -61,13 +64,15 @@ class DBSCAN():
             idx += 1
 
 if __name__ == "__main__":
-    X, y_true = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
+    X, y_true = make_moons(n_samples=300, noise=0.05, random_state=0)
+    
+    X = StandardScaler().fit_transform(X)
 
-    model = DBSCAN(eps=0.6, min_samples=4)
-    model.fit(X)
+    model = DBSCAN(eps=0.3, min_samples=5, metrics="euclidian")
+    model.fit_predict(X)
 
     unique_labels = set(model.labels)
-    colors = plt.cm.get_cmap("Spectral")(np.linspace(0, 1, len(unique_labels)))
+    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
 
     for k, col in zip(unique_labels, colors):
         if k == -2:
@@ -75,7 +80,7 @@ if __name__ == "__main__":
 
         class_member_mask = (model.labels == k)
         xy = X[class_member_mask]
-        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=6)
+        plt.scatter(xy[:, 0], xy[:, 1], color=tuple(col), edgecolor='k', s=50)
 
-    plt.title("DBSCAN: Clusters Identificados e Ruído (Preto)")
+    plt.title(f"DBSCAN - Clusters: {len(unique_labels) - (1 if -2 in unique_labels else 0)}")
     plt.show()
